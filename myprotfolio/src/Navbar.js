893532e11f9de3baaf8ac3ee4d9883bfc8e16473
Vaibhav Prakash <vaibhav.prakash.mail@gmail.com>
2021-01-12 00:08:10 +0530

    nav update

--- myprotfolio/src/Navbar.js
+++ myprotfolio/src/Navbar.js
@@ -5,44 +5,53 @@ import Navpart from './Navpart';
 class Navbar extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            Link:fun(){
-                if(document.getElementById("navLink2").className==="navLinkSpanSmall"){
-                    return "navLinkSpanSmallToggle";
-                }
-                else if(document.getElementById("navLink2").className==="navLinkSpanSmallToggle"){
-                    return "navLinkSpanSmall";
-                }
-                else{
-                    return "navLinkSpan";
-                }
-            }
-        };
-    }
-    
-    componentDidMount() {
-        let x=window.innerWidth;
-        if(x<=768){
-            this.setState({
-                Link:"navLinkSpanSmall"
-            });
+        if(window.innerWidth <=768){
+            this.state={link:"navLinkSpanSmall"};
         }
         else{
-            this.setState({
-                Link:"navLinkSpan"
-            });
+            this.state={link:"navLinkSpan"};
         }
+        this.fun=this.fun.bind(this);
+        this.funt=this.funt.bind(this);
     }
     
+    fun(){
+        if(document.getElementById("navLink2").className==="navLinkSpanSmall"){
+            this.setState({link:"navLinkSpanSmallToggle"});
+        }
+        else if(document.getElementById("navLink2").className==="navLinkSpanSmallToggle"){
+            this.setState({link:"navLinkSpanSmall"});
+        }
+        else{
+            this.setState({link:"navLinkSpan"});
+        }
+    }
+
+    funt(){
+        let x=window.innerWidth;
+        if(x<=768){
+            this.setState({link:"navLinkSpanSmall"});
+        }
+        else{
+            this.setState({link:"navLinkSpan"})
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize',this.funt);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize',this.funt);
+    }
     render(){
         
             return (
             <nav className="nav">
-                <Navpart class={this.state.Link} id="navLink1" href="#" text="About Me" />
-                <Navpart id="navLink2" href="#" text="My Skills" />
-                <Navpart id="navLink3" href="#" text="My Experience" />
-                <Navpart id="navLink4" href="#" text="My Projecs" />
-                <button id="navIcon" onClick={this.state.fun}><FaAlignJustify color='grey' size='20px' /></button>
+                <Navpart class={this.state.link} id="navLink1" href="#" text="About Me" />
+                <Navpart class={this.state.link} id="navLink2" href="#" text="My Skills" />
+                <Navpart class={this.state.link} id="navLink3" href="#" text="My Experience" />
+                <Navpart class={this.state.link} id="navLink4" href="#" text="My Projecs" />
+                <span id="navIcon" onClick={this.fun}><FaAlignJustify color='grey' size='30px' /></span>
             </nav>
         );
     }
--- myprotfolio/src/Navpart.js
+++ myprotfolio/src/Navpart.js
@@ -4,7 +4,7 @@ import {FaWalking,FaTools,FaBook,FaUserTie} from "react-icons/fa";
 
 function Navpart(props){
     return (
-        <span className="navLinkSpan" id={props.id}>
+        <span className={props.class} id={props.id}>
             {props.id==='navLink1'?<FaUserTie color='grey' size='20px' />:props.id==='navLink2'?<FaTools color='grey' size='20px' />:props.id==='navLink3'?<FaWalking color='grey' size='20px' />:<FaBook color='grey' size='20px' />}
             <a className="navLink" href={props.href}>{props.text}</a>
         </span>
