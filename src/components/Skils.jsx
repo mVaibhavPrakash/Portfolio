1f36d58e71ed0cbe16b23c5021c2475981c38d03
Vaibhav prakash <vp1997dav@gmail.com>
2022-07-31 02:05:53 +0530

    added skills carousel

--- src/components/Skils.jsx
+++ src/components/Skils.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect,useRef } from 'react'
+import { useEffect,useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons'
 import Carousel from '../js/carousel'
